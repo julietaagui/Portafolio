8096ae4671ca2ba7bba15cbf176cef59422290c1
julietaagui <aguilerajulieta700@gmail.com>
2025-01-13 18:16:28 -0300

    corrigiendo cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './App.css'
+import App from './App';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
-
-
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
